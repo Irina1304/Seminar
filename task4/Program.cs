2540300928ae97bf31ab196756fff77feedd0fe5
Irina <migalina.i@icloud.com>
2022-09-20 01:59:42 +0700

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает одно число n, а на выход показывает все целые числа в промежутке от -n до n.
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = -number; i<=number; i++)
+Console.WriteLine($"{i} ");
