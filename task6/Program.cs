1d59adea8817248baa8f8c87ecbdd62cfb509a20
irina <migalina.i@icloud.com>
2022-09-24 13:56:40 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ Напишите программ, которая выводит случайное число из отрезка и показывает наибольшую цифру числа.
+78 - 8
+12 - 2
+85 -8
+*/
+
+int GetRandomNumber(int leftRange, int rightRange)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(leftRange, rightRange);
+    return result;
+}
+int GetMaxDigitOfNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit > secondDigit)
+        return firstDigit;
+    else
+        return secondDigit;
+}
+
+int number = GetRandomNumber(10, 100);
+Console.WriteLine($"Рандомное число: {number}");
+
+int maxDigit = GetMaxDigitOfNumber(number);
+Console.WriteLine($"Максимальная цифра: {maxDigit}");
