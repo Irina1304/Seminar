0680c5088a3508a70879aa75ca131ee0917888cd
irina <migalina.i@icloud.com>
2022-09-27 11:53:39 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+
+int GetNumber ()
+{
+    Console.WriteLine("Введите два числа");
+    string firstNumberStr = Console.ReadLine();
+    int firstNumber = int.Parse(firstNumberStr);
+    return firstNumber;
+}
+
+int firstNumber = GetNumber();
+int secondNumber = GetNumber();
+firstNumber = Math.Abs(firstNumber);
+secondNumber = Math.Abs(secondNumber);
+
+if (firstNumber / secondNumber == secondNumber || secondNumber / firstNumber == firstNumber)
+    Console.WriteLine ("Да");
+else 
+    Console.WriteLine ("Нет");
